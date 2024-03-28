7fb9f49dbda15b6210d83b3e159746a3071600fe
DivDE <divde@musicociel.fr>
2024-03-28 13:49:31 +0100

    Allow customizing the base image and commit options in installAndRun

--- src/alpine/installAndRun.ts
+++ src/alpine/installAndRun.ts
@@ -1,25 +1,31 @@
 import { Writable } from "stream";
-import { temporaryContainer } from "../container";
+import { CommitOptions, temporaryContainer } from "../container";
 import { ImageBuilder } from "../imageBuilder";
 import { CacheOptions, apkAdd } from "./apkAdd";
 
 export interface InstallAndRunOptions {
+  baseImage?: string;
   apkPackages: string[];
   command: string[];
   buildahRunOptions: string[];
   cacheOptions?: CacheOptions;
+  commitOptions?: CommitOptions;
   logger?: Writable;
 }
 
 export const installAndRun = async ({
+  baseImage = "alpine",
   apkPackages,
   command,
   buildahRunOptions,
   cacheOptions,
+  commitOptions,
   logger,
 }: InstallAndRunOptions) => {
-  const imageBuilder = await ImageBuilder.from("alpine", {
+  const imageBuilder = await ImageBuilder.from(baseImage, {
     containerCache: cacheOptions?.containerCache,
+    commitOptions,
+    logger,
   });
   await imageBuilder.executeStep(
     apkAdd(apkPackages, { apkCache: cacheOptions?.apkCache }),
