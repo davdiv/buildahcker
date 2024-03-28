26cb7ecb5ab0708c17d7648450f6be2f67b27a80
DivDE <divde@musicociel.fr>
2024-03-28 22:24:54 +0100

    Fixing and improving writePartitions

--- src/alpine/partitions/writePartitions.ts
+++ src/alpine/partitions/writePartitions.ts
@@ -1,5 +1,4 @@
-import { close } from "fs";
-import { open } from "fs/promises";
+import { close, createReadStream, createWriteStream, open } from "fs";
 import { pipeline } from "stream/promises";
 
 export interface OffsetAndSize {
@@ -7,11 +6,20 @@ export interface OffsetAndSize {
   size: number;
 }
 
-export interface PartitionConfig {
+export interface PartitionConfigFromFile {
   inputFile: string;
   input?: OffsetAndSize;
   output: OffsetAndSize;
 }
+
+export interface PartitionConfigFromBuffer {
+  inputBuffer: Buffer;
+  output: OffsetAndSize;
+}
+
+export type PartitionConfig =
+  | PartitionConfigFromBuffer
+  | PartitionConfigFromFile;
 
 export interface WritePartitionsOptions {
   outputFile: string;
@@ -19,33 +27,45 @@ export interface WritePartitionsOptions {
 }
 
 export const writePartitions = async (config: WritePartitionsOptions) => {
-  const outputFile = await open(config.outputFile, "r+");
+  const outputFile = await new Promise<number>((resolve, reject) =>
+    open(config.outputFile, "r+", (error, fd) =>
+      error ? reject(error) : resolve(fd),
+    ),
+  );
   try {
     for (const partition of config.partitions) {
-      const inputFile = await open(partition.inputFile, "r");
-      try {
+      const outputStream = createWriteStream("", {
+        fd: outputFile,
+        start: partition.output.offset,
+        autoClose: false,
+      });
+      if ("inputFile" in partition) {
         const inputStart = partition.input?.offset ?? 0;
-        const inputStream = inputFile.createReadStream({
+        const inputStream = createReadStream(partition.inputFile, {
           start: inputStart,
           end:
             inputStart +
             Math.min(partition.output.size, partition.input?.size ?? Infinity),
           autoClose: false,
         });
-        const outputStream = outputFile.createWriteStream({
-          start: partition.output.offset,
-          autoClose: false,
-        });
-        await pipeline(inputStream, outputStream);
-      } finally {
-        await inputFile.close();
+        try {
+          await pipeline(inputStream, outputStream);
+        } finally {
+          await inputStream.close();
+        }
+      } else {
+        await new Promise<void>((resolve, reject) =>
+          outputStream.write(
+            partition.inputBuffer.subarray(
+              0,
+              Math.min(partition.output.size, partition.inputBuffer.length),
+            ),
+            (error) => (error ? reject(error) : resolve()),
+          ),
+        );
       }
     }
   } finally {
-    // FIXME: we should do:
-    // await outputFile.close();
-    // but it causes node.js to exit (cf https://github.com/nodejs/node/issues/48466)
-    // so instead we do:
-    await new Promise((resolve) => close(outputFile.fd, resolve));
+    await new Promise((resolve) => close(outputFile, resolve));
   }
 };
--- test/partitions.test.ts
+++ test/partitions.test.ts
@@ -1,4 +1,4 @@
-import { stat } from "fs/promises";
+import { readFile } from "fs/promises";
 import { join } from "path";
 import { expect, it } from "vitest";
 import { exec } from "../src";
@@ -20,8 +20,13 @@ it(
       outputFile,
       partitions: [
         {
-          name: "mypart",
-          size: 1000000,
+          name: "p1",
+          size: 10000,
+          type: PartitionType.LinuxData,
+        },
+        {
+          name: "p2",
+          size: 10000,
           type: PartitionType.LinuxData,
         },
       ],
@@ -31,14 +36,23 @@ it(
     await writePartitions({
       outputFile,
       partitions: [
-        {
-          inputFile: __filename,
-          output: partitions[0],
-        },
+        { inputFile: __filename, output: partitions[0] },
+        { inputBuffer: Buffer.from("Hello!!", "utf8"), output: partitions[1] },
       ],
     });
-    const outputImg = await stat(outputFile);
-    expect(outputImg.isFile()).toBeTruthy();
-    expect(outputImg.size).toBeGreaterThan(1000000);
+    const thisFile = await readFile(__filename);
+    const outputImg = await readFile(outputFile);
+    expect(outputImg.subarray(512, 512 + 8).toString("ascii")).toBe("EFI PART");
+    expect(
+      outputImg.subarray(
+        partitions[0].offset,
+        partitions[0].offset + thisFile.length,
+      ),
+    ).toEqual(thisFile);
+    expect(
+      outputImg
+        .subarray(partitions[1].offset, partitions[1].offset + 7)
+        .toString("utf8"),
+    ).toBe("Hello!!");
   },
 );
