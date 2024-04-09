66a1be8d48d537d2d4bb8dc14239f3266b816d83
DivDE <divde@musicociel.fr>
2024-04-10 00:48:14 +0200

    Improving sshKeygen utility

--- src/alpine/ssh/index.ts
+++ src/alpine/ssh/index.ts
@@ -4,6 +4,7 @@ import type { Writable } from "stream";
 import type { ImageOrContainer } from "../../container";
 import { withImageOrContainer } from "../../container";
 import type { ContainerCache } from "../../containerCache";
+import { DiskFile } from "../../steps";
 import { prepareApkPackages } from "../prepareApkPackages";
 
 export type SSHKeyType =
@@ -16,37 +17,58 @@ export type SSHKeyType =
 
 export const defaultSSHKeyGenTypes: SSHKeyType[] = ["ed25519"];
 
-export interface SSHKeyGenConfig {
+export interface SSHKeyGenConfig<
+  T extends SSHKeyType,
+  P extends string,
+  S extends string,
+> {
   outputFolder: string;
-  types?: SSHKeyType[];
+  types?: T[];
+  prefix?: P;
+  suffix?: S;
   opensshSource?: ImageOrContainer;
   containerCache?: ContainerCache;
   apkCache?: string;
   logger?: Writable;
 }
 
-export const sshKeygen = async ({
+export const sshKeygen = async <
+  T extends SSHKeyType = "ed25519",
+  P extends string = "ssh_host_",
+  S extends string = "_key",
+>({
   outputFolder,
   opensshSource,
-  types = defaultSSHKeyGenTypes,
+  types = defaultSSHKeyGenTypes as any as T[],
+  prefix = "ssh_host_" as any as P,
+  suffix = "_key" as any as S,
   containerCache,
   apkCache,
   logger,
-}: SSHKeyGenConfig) => {
+}: SSHKeyGenConfig<T, P, S>): Promise<
+  Record<`${P}${T}${S}.pub` | `${P}${T}${S}`, DiskFile>
+> => {
+  const files: Record<`${P}${T}${S}.pub` | `${P}${T}${S}`, DiskFile> =
+    {} as any;
   await mkdir(outputFolder, { recursive: true });
   let hasMissingTypes = false;
   const keyTypesInfo = await Promise.all(
     types.map(async (type) => {
-      const keyFile = `ssh_host_${type}_key`;
+      const privKeyFileName: `${P}${T}${S}` = `${prefix}${type}${suffix}`;
+      const pubKeyFileName = `${privKeyFileName}.pub`;
       let alreadyPresent = false;
+      const pubKeyFilePath = join(outputFolder, pubKeyFileName);
+      const privKeyFilePath = join(outputFolder, privKeyFileName);
+      files[privKeyFileName] = new DiskFile(privKeyFilePath, { mode: 0o600 });
+      files[pubKeyFileName] = new DiskFile(pubKeyFilePath, { mode: 0o600 });
       try {
         const [pubKeyFileStat, privKeyFileStat] = await Promise.all([
-          stat(join(outputFolder, `${keyFile}.pub`)),
-          stat(join(outputFolder, keyFile)),
+          stat(pubKeyFilePath),
+          stat(privKeyFilePath),
         ]);
         if (!pubKeyFileStat.isFile() || !privKeyFileStat.isFile()) {
           throw new Error(
-            `${keyFile}.pub or ${keyFile} exists in ${outputFolder} and is not a regular file.`,
+            `${privKeyFileName}.pub or ${privKeyFileName} exists in ${outputFolder} and is not a regular file.`,
           );
         }
         alreadyPresent = true;
@@ -56,7 +78,7 @@ export const sshKeygen = async ({
         }
         hasMissingTypes = true;
       }
-      return { alreadyPresent, keyFile, type };
+      return { alreadyPresent, keyFile: privKeyFileName, type };
     }),
   );
   if (hasMissingTypes) {
@@ -91,4 +113,5 @@ export const sshKeygen = async ({
       { logger },
     );
   }
+  return files;
 };
