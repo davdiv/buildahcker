90e94f8508900613bfe1f2c534d4e7bc005fc956
DivDE <divde@musicociel.fr>
2025-05-21 22:46:49 +0200

    addRootDirectory

--- src/steps/files/step.ts
+++ src/steps/files/step.ts
@@ -1,23 +1,43 @@
 import { createHash } from "crypto";
 import { mkdir, rm } from "fs/promises";
-import type { AtomicStep } from "../../container";
+import type { AtomicStep, Container } from "../../container";
 import { resolveParentInContainer } from "../../resolveInContainer";
-import type { DirectoryContent } from "./base";
+import type { CopyableFile, DirectoryContent } from "./base";
 import {
   hashDirectoryContent,
   normalizeDirectoryEntries,
   normalizeRelativePath,
 } from "./base";
 import { dirname } from "path";
+import { DiskDirectory } from "./disk";
+
+const writeEntriesToContainer = async (
+  entries: Map<string, CopyableFile>,
+  container: Container,
+) => {
+  for (const [filePath, file] of entries) {
+    const destination = await container.resolve(filePath);
+    await mkdir(dirname(destination), { recursive: true });
+    await file.writeTo(destination);
+  }
+};
+
+export const addRootDirectory = (path: string) => {
+  const directory = new DiskDirectory(path, { uid: 0, gid: 0, mode: 0o755 }); // note that attributes are ignored
+  const step: AtomicStep = async (container) => {
+    await writeEntriesToContainer(await directory.getContent(), container);
+  };
+  step.getCacheKey = async () => {
+    const hash = (await directory.getContentHash()).toString("base64url");
+    return `ADD-ROOT-DIRECTORY-${hash}`;
+  };
+  return step;
+};
 
 export const addFiles = (files: DirectoryContent) => {
   const entries = normalizeDirectoryEntries(files);
   const step: AtomicStep = async (container) => {
-    for (const [filePath, file] of entries) {
-      const destination = await container.resolve(filePath);
-      await mkdir(dirname(destination), { recursive: true });
-      await file.writeTo(destination);
-    }
+    await writeEntriesToContainer(entries, container);
   };
   step.getCacheKey = async () => {
     const hash = (await hashDirectoryContent(entries)).toString("base64url");
